a1a820714c7da964dd2372ad5bd123046c5aaf31
sriram18 <sriram.r@surveysparrow.com>
2022-06-13 13:18:27 +0530

    third

--- client/src/components/getTodo.js
+++ client/src/components/getTodo.js
@@ -37,7 +37,16 @@ const gettodo=async()=>{
       <div className='GetTodo'>
           <Nav1/>
           <br />
-          {(todo.tasks.length===0)?<h2>No Tasks to Complete</h2>:todo.tasks.map((item)=><div className='tasks'  ><p className='para'><b><i>{(item.task).toUpperCase()}</i></b><button className='btn-btn' onClick={async()=>{const resp=await Axios.post('http://localhost:3000/api/deletetask',{id:item.id});console.log(resp);gettodo().then((arr)=>{setTodo({tasks:arr})})}}><b>X</b></button></p><br /></div>)}
+          {
+          (todo.tasks.length===0)?
+          <h2>No Tasks to Complete</h2>:
+          todo.tasks.map((item)=>
+          <div className='tasks'  >
+              <p className='para'><b><i>{(item.task).toUpperCase()}</i></b><button className='btn-btn' onClick={async()=>{const resp=await Axios.post('http://localhost:3000/api/deletetask',{id:item.id});console.log(resp);gettodo().then((arr)=>{setTodo({tasks:arr})})}}><b>X</b></button></p>
+              <br />
+          </div>
+          )
+          }
       </div>  
     )
 }
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,16 +1,78 @@
 import './home.css'
 import { Link} from 'react-router-dom';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Nav from './nav'
 import { useSelector } from 'react-redux';
 import {useNavigate} from 'react-router-dom';
 
 function Home() {
+  
+  let currentdate = new Date(); 
+  let s=''
+  let m=''
+  let h=''
+  if(currentdate.getSeconds()<10){
+    s='0'+currentdate.getSeconds()
+  }
+  else{
+    s=currentdate.getSeconds()
+  }
+  if(currentdate.getMinutes()<10){
+    m='0'+currentdate.getMinutes()
+  }
+  else{
+    m=currentdate.getMinutes()
+  }
+  if(currentdate.getHours()<10){
+    h='0'+currentdate.getHours()
+  }
+  else{
+    h=currentdate.getHours()
+  }
+  let time = h + " : "  
+            + m + " : " 
+            + s
+    setInterval(function(){
+      let currentdate = new Date(); 
+      let s='';
+      let m='';
+      let h='';
+      if(currentdate.getSeconds()<10){
+        s='0'+currentdate.getSeconds()
+      }
+      else{
+        s=currentdate.getSeconds()
+      }
+      if(currentdate.getMinutes()<10){
+        m='0'+currentdate.getMinutes()
+      }
+      else{
+        m=currentdate.getMinutes()
+      }
+      if(currentdate.getHours()<10){
+        h='0'+currentdate.getHours()
+      }
+      else{
+        h=currentdate.getHours()
+      }
+      let time = h + " : "  
+                + m + " : " 
+                + s
+      document.getElementById('t').innerHTML=time;
+    },1000);
+  
+  
     return (
         <div className="Home">
         <Nav/> 
         <h1><i>HELLO! :)</i></h1>
         <h3><i>Welcome. If you already have an account here sign in to continue or else sign up first.</i></h3>
+        <br />
+        <br />
+        <div style={{textAlign : 'center'}}>
+        <h2 ><b>TIME : </b></h2>
+        <h2 id='t' >{time}</h2>
+        </div>
     </div>
     );
   }
